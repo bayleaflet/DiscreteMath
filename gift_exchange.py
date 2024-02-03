19b319a3de670730cd554684cbfc7237ad9d0549
bayleaflet <bayleejchristensen@gmail.com>
2024-02-02 19:52:56 -0800

    First Step Complete
    Initialized main function in gift_exchange.py that is working to
    this point. Is doing a simple test right now to make sure formats are
    working. Overrode the do_calculation with a dummy list, and will need to
    change that going forward.

--- gift_exchange.py
+++ gift_exchange.py
@@ -1,8 +1,31 @@
-# BJC, Original Author, 2/2024
+#BJC, Original Author, 2/2024
 # Program that considers chances for a holiday gift exchange of a user getting
 # themself. Uses a permutation generator to do so.
 
-import permutation_generator
+from permutation_gen import *
+
+def do_calculation(infix):
+    perm_list = consider_number(infix)
+    all_perms = generate_all_permutations(perm_list)
+    # Could return a list as [3 (wins), 6 (tries)]
+    test =[3,6]
+    return test
 
 def main():
 
+    # Gets input from user
+    while True:
+        amt_of_students = int(input("Please pick a number from 2 to 10: "))
+        if amt_of_students > 10 or amt_of_students < 2:
+            print("Sorry, only choose a number between 2 and 10")
+        else:
+            break
+
+    odds = do_calculation(amt_of_students)
+    wins = odds[0]
+    tries = odds[1] # Count of Total Permutations
+
+    print(f"{wins} of {tries} wins. Probability of a person getting themself is", wins / tries)
+
+if __name__ == '__main__':
+    main()
