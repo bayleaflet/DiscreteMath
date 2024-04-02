fcc913b2a732a435ef82f7bea201748fe280afbf
bayleaflet <bayleejchristensen@gmail.com>
2024-04-01 17:40:38 -0700

    Small additions -- It only accepts strings that are long enough for RSA encrytion, and it later mods them so they aren't too long for calculation, then it checks if they are even or odd, if even adds one to make odd. `

--- RSA_Assn/RSA.py
+++ RSA_Assn/RSA.py
@@ -8,9 +8,26 @@ class RSA:
     def __init__(self):
         self.alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    def generate_keys(self, string1, string2):
-        p = self.to_base_10(string1)
-        q = self.to_base_10(string2)
+    def generate_keys(self):
+        while True:
+            string1 = input("Please enter first string: ")
+            string2 = input("Please enter second string: ")
+            p = self.to_base_10(string1)
+            q = self.to_base_10(string2)
+            if p >= 10**200 and q >= 10**200:
+                break
+            else:
+                print("Your input strings are too short. Try again.")
+                continue
+        p = p % 10 ** 200 # Mod so they aren't too long
+        q = q % 10 ** 200
+        # Make sure p and q are odd
+        if p % 2 == 0:
+            p += 1
+        if q % 2 == 0:
+            q += 1
+
+
         return p, q
 
     def encrypt(self, input_file, output_file):
@@ -32,6 +49,6 @@ text1 = "verylongtextstring1"
 text2 = "anotherverylongtextstring2"
 
 rsa_instance = RSA()
-p, q = rsa_instance.generate_keys(text1,text2)
+p, q = rsa_instance.generate_keys()
 print("p:", p)
 print("q:", q)
