caedf24bc66cecf88aa65529cb341b234dde7871
bayleaflet <bayleejchristensen@gmail.com>
2024-04-03 17:42:01 -0700

    Untested functions

--- RSA_Assn/RSA.py
+++ RSA_Assn/RSA.py
@@ -9,6 +9,8 @@ class RSA:
 
     def __init__(self):
         self.alphabet = "abcdefghijklmnopqrstuvwxyz"
+        self.encrypt_alphabet = ".,?! \t\n\rabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        self.block_size = math.ceil(math.log(10**400, len(self.encrypt_alphabet)))
 
     def generate_keys(self):
         while True:
@@ -70,7 +72,46 @@ class RSA:
         return p, q
 
     def encrypt(self, input_file, output_file):
-        pass
+        # In main you will use Encrypt to encrypt your file
+        # Open input file in binary mode, read contents
+        with open(input_file, "rb") as fin:
+            plain_text_binary = fin.read()
+
+        # Decode binary data into a Unicode str using UTF-8 encoding
+        plain_text = plain_text_binary.decode("utf-8")
+
+        # Breaks down plaintext into blocks no bigger than block_size
+        blocks = []
+        for i in range(0, len(plain_text), self.block_size):
+            block = plain_text[i:i+self.block_size]
+            blocks.append(block)
+
+        # Convert blocks into a Base 10 number
+        numbers = []
+        for block in blocks:
+            base_10_num = self.to_base_10(block, self.encrypt_alphabet)
+            numbers.append(base_10_num)
+
+        # Read n and e from public.txt
+        with open("public.txt", "r") as public_file:
+            n = int(public_file.readline().strip())
+            e = int(public_file.readline().strip())
+
+        # Encrypt each base 10 num using RSA
+        encrypted_blocks = []
+        for number in numbers:
+            encrypted_number = pow(number, e, n)
+            encypted_blocks.append(encrypted_number)
+
+        # Convert the resulting integers back to the base 70 alphabet
+        encrypted_text = ''.join([self.to_base_10(num, self.encrypt_alphabet) for num in encrypted_blocks])
+
+        # Add block separators ($) after each block
+        encrypted_text_with_blocks = '$'.join(encrypted_text[i:i+self.block_size] for i in range(0, len(encrypted_text), self.block_size))
+
+        # Write resulting numbers to output file
+        with open(output_file, "w") as fout:
+            fout.write(encrypted_text_with_blocks.encode("utf-8"))
 
     def decrypt(self, input_file, output_file):
         pass
